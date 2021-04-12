07ac86f6308c0452922e375ea18b555766659bd6
weldon <wxh199311@outlook.com>
2021-04-12 10:13:43 +0800

    完成了注册登录功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,7 +13,7 @@ $(function () {
   });
 
   // 定义校验规则
-  const { form } = layui;
+  const { form, layer } = layui;
   // const form = layui.form;
   form.verify({
     pwd: [/^[\S]{6,12}$/, '密码不符合规则'],
@@ -31,7 +31,7 @@ $(function () {
     e.preventDefault();
     $.ajax({
       type: 'POST',
-      url: 'http://api-breakingnews-web.itheima.net/api/reguser',
+      url: '/api/reguser',
       data: {
         username: $('.reg-box [name=username]').val(),
         password: $('.reg-box [name=password]').val(),
@@ -39,10 +39,41 @@ $(function () {
 
       success(res) {
         if (res.status !== 0) {
-          return console.log(res.message);
+          return layer.msg(res.message);
         }
-        console.log('注册成功');
+        layer.msg('注册成功');
+        $('#link_login').click();
+      },
+    });
+  });
+
+  // 监听登录表单的提交事件
+  $('#form_login').submit(function (e) {
+    // debugger;
+    // 阻止默认提交行为
+    e.preventDefault();
+    $.ajax({
+      url: '/api/login',
+      method: 'POST',
+      // 快速获取表单中的数据
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('登录失败！');
+        }
+        layer.msg('登录成功！');
+        // 将登录成功得到的 token 字符串，保存到 localStorage 中
+        localStorage.setItem('token', res.token);
+        // 跳转到后台主页
+        location.href = '/index.html';
       },
     });
   });
 });
+
+// github
+// login 提交本地仓库
+// login 提交远程仓库 ？
+// login合并到main(主分支)
+// master提交到远程仓库
+//
